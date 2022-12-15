cec040bd392c821ad105ff238dbd30c082903fee
BrunoUemura <bruno.h.uemura@outlook.com>
2022-12-15 01:11:55 -0300

    feat: adds routes to controller

--- notifications-service/src/infra/http/http-module.ts
+++ notifications-service/src/infra/http/http-module.ts
@@ -3,10 +3,22 @@ import { Module } from '@nestjs/common';
 import { SendNotification } from '@application/usecases/send-notification';
 import { DatabaseModule } from '../database/database-module';
 import { NotificationsController } from './controllers/notifications-controller';
+import { CacncelNotification } from '@application/usecases/cancel-notification';
+import { CountRecipientNotifications } from '@application/usecases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@application/usecases/get-recipient-notifications';
+import { ReadNotification } from '@application/usecases/read-notification';
+import { UnreadNotification } from '@application/usecases/unread-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CacncelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
